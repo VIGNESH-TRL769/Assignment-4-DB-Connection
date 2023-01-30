c2d490c0f8eee53645e65fd84924c78491dec8bf
VIGNESH-TRL769 <123439677+VIGNESH-TRL769@users.noreply.github.com>
2023-01-30 17:54:52 +0530

    Update DatabaseConnection.java

--- src/main/java/org/example/DatabaseConnection.java
+++ src/main/java/org/example/DatabaseConnection.java
@@ -67,24 +67,31 @@ class DatabaseConnection {
         Scanner input=new Scanner(System.in);
 
         while(true) {
-            LOGGER.info("\n1.Connetion to DataBase\n2.new Connection\n3.Close All Connection\n4.View Status\n5.Exit\nEnter the Choice:");
-            int key = input.nextInt();
-            int temp=0;
-            Database.getInstance();
-            switch (key) {
-                case 1 -> Database.getConnection();
-                case 2 -> Database.newConnection();
-                case 3 -> Database.closeConnection();
-                case 4->  Database.viewStatus();
-                case 5 -> {
-                    LOGGER.info("-----Thank You-----");
-                    temp = 1;
+            try {
+                LOGGER.info("\n1.Connetion to DataBase\n2.new Connection\n3.Close All Connection\n4.View Status\n5.Exit\nEnter the Choice:");
+                int key = input.nextInt();
+                int temp = 0;
+                Database.getInstance();
+                switch (key) {
+                    case 1 -> Database.getConnection();
+                    case 2 -> Database.newConnection();
+                    case 3 -> Database.closeConnection();
+                    case 4 -> Database.viewStatus();
+                    case 5 -> {
+                        LOGGER.info("-----Thank You-----");
+                        temp = 1;
+                    }
+                    default -> LOGGER.info("Please enter Valid Choice");
                 }
-                default -> LOGGER.info("Please enter Valid Choice");
+                if (temp == 1) {
+                    break;
+                }
+
             }
-            if(temp==1)
-            {
-                break;
+            catch (Exception e) {
+                String value = "Please Enter Valid Numbers:" + e;
+                LOGGER.info(value);
+                input.nextLine();
             }
         }
 
